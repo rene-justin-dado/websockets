4e2e4f86464d6816b80f6cec3adc90f653e589af
Justin <justindado@yahoo.com>
2016-10-29 19:05:40 +1300

    server informs of connection and disconnection

---
+++ server.js
@@ -0,0 +1,21 @@
+import express from 'express'
+import { Server } from 'http'
+const app = express(),
+      http = Server(app),
+      io = require('socket.io')(http),
+      port = process.env.PORT || 4444
+
+// import { Server as server } from 'ws'
+// const socket = new server({ port: 5000 })
+app.get('/', (req, res) => res.sendFile(`${__dirname}/chat-app.html`))
+
+io.on('connection', socket => {
+  console.log('Someone connected! :D')
+  socket.emit('Hello')
+
+  socket.on('chat message', message => io.emit(`Chat message: ${message}`))
+
+  socket.on('disconnect', () => console.log('Somebody left! D:'))
+})
+
+http.listen(port, () => console.log('Listening on 4444'))
